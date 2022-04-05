a1cc0cb626a6441d86457f4e70e5c48d3ab31293
oneal.oh <oneal.oh@kakaomobility.com>
2022-04-05 21:21:45 +0900

    udpate: mocha test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,14 +3,26 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); // 요놈이 대문자 Web3인 이유는, web3 라이브러리의 constructor이기 때문,  instance를 만들기 위해 사용, 컨벤션인데 생성자 함수를 쓸때 대문자 씀, 일종의 클래스라고 생각해라
 const web3 = new Web3(ganache.provider()); // Web3 객체에 가나쉬 프로바이더와 연결해라, 우리가 연결하고자 하는 네트워크에 따라 달라짐
+const { interface, bytecode } = require('../compile');
 
-beforeEach(() => {
+let accounts;
+let inbox;
+
+beforeEach( async () => {
     // get a list of all accounts
-
+    accounts = await web3.eth.getAccounts();
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['hi, there']})
+        .send({ from: accounts[0], gas: '1000000'});
 
     // use one of those accounts to deploy the contract
 })
 
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log('inbox:', inbox);
+    });
+});
 
 
 
