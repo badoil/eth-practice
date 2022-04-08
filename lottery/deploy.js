446f14d2f0a795274a9d23db58653219c150a145
oneal.oh <oneal.oh@kakaomobility.com>
2022-04-08 18:06:24 +0900

    update: lottery-react

--- lottery/deploy.js
+++ lottery/deploy.js
@@ -3,10 +3,8 @@ const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
 const provider = new HDWalletProvider(
-  'REPLACE_WITH_YOUR_MNEMONIC',
-  // remember to change this to your own phrase!
-  'https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c'
-  // remember to change this to your own endpoint!
+  'cloud dice gold ignore estate portion keen carbon wall pen believe sudden',
+    'https://rinkeby.infura.io/v3/0c3faaaf0b984aa0b3a23ac42d6ad6f8'
 );
 const web3 = new Web3(provider);
 
@@ -20,6 +18,7 @@ const deploy = async () => {
     .send({ gas: '1000000', from: accounts[0] });
 
   console.log('Contract deployed to', result.options.address);
+  console.log('abi:', interface);
   provider.engine.stop();
 };
 deploy();
