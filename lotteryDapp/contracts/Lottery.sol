8d62de194efc38594372fd7d75603a275ecb834a
oneal.oh <oneal.oh@kakaomobility.com>
2022-04-18 14:05:53 +0900

    fix: Lotter.sol

--- lotteryDapp/contracts/Lottery.sol
+++ lotteryDapp/contracts/Lottery.sol
@@ -3,7 +3,15 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Lottery {
 
+    struct BetInfo {
+        uint256 answerBlockNumber;
+        address payable better;
+        bytes challenges;
+    }
+
     address public owner;
+
+    uint256 private _pot;
 
     constructor() public {
         owner = msg.sender;
@@ -11,5 +19,9 @@ contract Lottery {
 
     function getSomeValue() public pure returns(uint256 value) {
         return 5;
+    }
+
+    function getPot() public view returns(uint256 pot) {
+        return _pot;
     }
 }
