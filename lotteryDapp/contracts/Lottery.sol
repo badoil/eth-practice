e9b932e6c76f33e43d4d3f9eac9c48a68a5cfb10
oneal.oh <oneal.oh@kakaomobility.com>
2022-04-18 19:07:37 +0900

    update: Lottery.sol, function distribute()

--- lotteryDapp/contracts/Lottery.sol
+++ lotteryDapp/contracts/Lottery.sol
@@ -9,6 +9,8 @@ contract Lottery {
         bytes challenges;
     }
 
+    enum BlockStatus {Checkable, NotRevealed, BlockLimitPassed}
+
     uint256 private _head;          // queue 구조를 만들고, 이를 hashMap과 함께 사용
     uint256 private _tail;
     mapping(uint256 => BetInfo) private _bets;
@@ -37,17 +39,69 @@ contract Lottery {
      * @param challenges  유저가 보내는 1바이트 글자
      * @return 함수가 잘 수행되어쓴지 확인하는 boolean 값
      */
-    function bet(bytes memory challenges) public payable returns(bool) {
+    function bet(bytes memory challenges) public payable returns(bool) {    // 트랜잭션 기본 가스량 21000 가스
         // check the proper ith is sent
         require(msg.value == BET_AMOUNT, 'not enough money');
 
         // push bet to the queue
-        require(pushBet(challenges), "failed to add this bet info");
+        require(pushBet(challenges), "failed to add this bet info");    // 60000가스
 
         // emit event, 이벤트 로그는 블록체인 함수로 호출하고 따로 모을 수 있음, web3.js 로 로그를 긁어올 수 있음, 프론트 만들면서 알 수 있음
-        emit BET(_tail-1, msg.sender, msg.value, challenges, block.number+BET_BLOCK_INTERVAL);
+        emit BET(_tail-1, msg.sender, msg.value, challenges, block.number+BET_BLOCK_INTERVAL);  // emit 자체가 375가스, 파라미터 등등 4~5000 가스 
 
         return true;
+    }
+
+    // distribute
+
+    function distribute() public {
+        uint256 cur;
+        BetInfo memory b;
+        BlockStatus currentBlockStatus;
+
+        for(cur=_head; cur<_tail; cur++) {
+            b = _bets[cur];
+            currentBlockStatus = getBlockStatus(b.answerBlockNumber);
+
+            //checkable: block.number > answerBlockNumber && block.number - BLOCK_LIMIT <= answerBlockNumber
+            if (currentBlockStatus == BlockStatus.Checkable) {
+                // if win, bettor gets pot money
+
+                // if fail, bettor's money goes to pot
+
+                // if draw, refund bettor's money
+            }
+            //not revealed
+            if (currentBlockStatus == BlockStatus.NotRevealed) {
+                break;
+            }
+            //block limit passed
+            if (currentBlockStatus == BlockStatus.BlockLimitPassed) {
+                //refund
+                //emit refund
+
+            }
+            popBet(cur);
+
+
+        }
+    }
+
+    function getBlockStatus(uint256 answerBlockNumber) internal view returns(BlockStatus) {
+        //  현재 블록에서, 큐에 담겨있는 베팅의 당첨 여부를 알 수 있는 경우
+        if (block.number > answerBlockNumber && block.number - BLOCK_LIMIT < answerBlockNumber) {
+            return BlockStatus.Checkable;
+        }
+
+        // 현재 블록이 생성중이라 당첨 여부를 알려 줄 수 없음
+        if (block.number <= answerBlockNumber) {
+            return BlockStatus.NotRevealed;
+        }
+
+        // 너무 옛날 블록에 적힌 베팅 정보라 당첨 여부를 알려 줄 수 없음
+        if (block.number > answerBlockNumber && block.number - BLOCK_LIMIT >= answerBlockNumber) {
+            return BlockStatus.BlockLimitPassed;
+        }
     }
 
     //distribute, check the answer, give the money to the winner
@@ -59,14 +113,14 @@ contract Lottery {
         challenges = b.challenges;
     }
 
-    function pushBet(bytes memory challenges) internal returns(bool) {    // 큐에 집어넣음
+    function pushBet(bytes memory challenges) internal returns(bool) {    // 큐에 집어넣음, 총 60000가스
         BetInfo memory b;
-        b.answerBlockNumber = block.number + BET_BLOCK_INTERVAL;
-        b.bettor = msg.sender;
-        b.challenges = challenges;
+        b.answerBlockNumber = block.number + BET_BLOCK_INTERVAL;    // 32bytes  20000 가스
+        b.bettor = msg.sender;                                      // 20bytes  20000 가스
+        b.challenges = challenges;                                  // byte
 
-        _bets[_tail] = b;
-        _tail++;
+        _bets[_tail] = b;   // 위의 변수들이 실제로 블록에 저장되는 연산
+        _tail++;            // 32bytes  20000 가스
 
         return true; 
     }
